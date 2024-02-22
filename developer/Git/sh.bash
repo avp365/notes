9752e4230af89670afdc1c1f745f233abd11c29e
Andy <avp365@yandex.ru>
2024-02-22 19:56:54 +0000

    правки

---
+++ developer/Git/sh.bash
@@ -0,0 +1,5 @@
+#
+git add . ; git commit -m "module12_01" ; git push
+git add . ; git commit -m "ci" ; git push
+git add . ; git commit -m "правки" ; git push
+git remote set-url origin https://gitlab.rebrainme.com/kubernetes_users_repos/1876/whoami
